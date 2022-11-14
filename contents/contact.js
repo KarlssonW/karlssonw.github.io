0e6f9bee63dac212c8d8f393158447b4f7dddcf4
Karlsson <qwertderStar@gmail.com>
2022-11-14 01:48:23 +0100

    Update contact.js

--- contents/contact.js
+++ contents/contact.js
@@ -1,5 +1,5 @@
 const contact = `
 <div class="about">
-	You can reach me at karlsson@winkels.berlin
+	You can reach me at <a href='m&#97;ilt&#111;&#58;%&#54;B&#97;rl%7&#51;so%6E%40&#119;inke&#108;&#37;73%&#50;&#69;berl&#37;69n'>&#107;arl&#115;so&#110;&#64;win&#107;&#101;ls&#46;berlin</a>
 </div>
 `
